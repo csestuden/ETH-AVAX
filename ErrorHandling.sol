e50b69a51987ae661bf36de17f204bd549514988
csestuden <97797369+csestuden@users.noreply.github.com>
2023-07-30 12:05:25 +0530

    Update ErrorHandling.sol

--- ErrorHandling.sol
+++ ErrorHandling.sol
@@ -21,3 +21,25 @@ contract ErrorHandling {
         value -= _decrement;
     }
 }
+
+contract ErrorHandlingDemo {
+    ErrorHandling public errorHandling;
+
+    constructor(address _errorHandlingAddress) {
+        errorHandling = ErrorHandling(_errorHandlingAddress);
+    }
+
+    function testErrorHandlers() public {
+        // Testing require
+        errorHandling.setValue(0); // This will trigger the require error
+        // The transaction will be reverted with the error message "Value must be greater than zero"
+
+        // Testing assert
+        errorHandling.incrementValue(uint(-1)); // This will trigger the assert error
+        // The transaction will be reverted due to the failing assert statement
+
+        // Testing revert
+        errorHandling.decrementValue(100); // This will trigger the revert error
+        // The transaction will be reverted with the error message "Decrement value exceeds current value"
+    }
+}
